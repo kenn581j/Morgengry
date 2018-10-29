967e9e66b65a9b750d91a26baa269bb066cc80cc
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2018-10-29 09:41:02 +0100

    færdig med første opgave.

---
+++ Morgengry/Utility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Utility
+    {
+        public static double GetValueOfBook(Book vBook)
+        {
+            return vBook.Price;
+        }
+
+        public static double GetValueOfAmulet(Amulet vAmulet)
+        {
+            double amuletValue = 0;
+            if (vAmulet.Quality == Level.low)
+            {
+                amuletValue = 12.5;
+            }
+            else if (vAmulet.Quality == Level.medium)
+            {
+                amuletValue = 20.0;
+            }
+            else if (vAmulet.Quality == Level.high)
+            {
+                amuletValue = 27.5;
+            }
+            return amuletValue;
+        }
+
+        public static double GetValueOfCourse(Course vCourse)
+        {
+            double getHours = vCourse.DurationInMinutes / 60.0;            
+            int fullHours = Convert.ToInt32(getHours);
+            double finalPrice = 875 * fullHours;            
+            return finalPrice;
+        }
+    }
+}
---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f3941a5-a628-4a56-b966-2f21245e104f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
